9319e0490f8882c19b998b4cdaaed81b0338226c
Lavish Yadav <lavish99038@gmail.com>
2025-05-31 22:24:30 +0530

    chore: install and configure Tailwind CSS

---
+++ frontend/pages/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-indigo-400  text-blue-300">
+      <h1 className="text-5xl font-bold">TAILWIND WORKS! 🎉</h1>
+    </div>
+  );
+}
